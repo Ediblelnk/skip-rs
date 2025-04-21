f2c288445cd55555ee99d306d7369c410a3893d4
Ediblelnk <peters1731@gmail.com>
2025-04-21 12:54:49 -0400

    plans

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # skip-rs
 Implementation of a skip list in rust.
+
+## Methods to Implement Still
+
+- [ ] `remove_at_index`
+- [ ] `key_at_index`
+- [ ] `edit_at_index`
+- [ ] `pop_front`
+- [ ] `pop_back`
--- skip-rs/src/main.rs
+++ skip-rs/src/main.rs
@@ -4,21 +4,21 @@ fn main() -> Result<(), String> {
     let mut skip_list = SkipList::new();
 
     for i in 0..5 {
-        let s = format!("as");
-        skip_list.insert(i * 2 + 1, s);
+        // let s = format!("as");
+        skip_list.insert(i * 2 + 1, ());
     }
     for i in 0..5 {
-        let s = format!("as");
-        skip_list.insert(i * 2, s);
+        // let s = format!("as");
+        skip_list.insert(i * 2, ());
     }
 
-    println!("Skip List:\n{}", skip_list);
+    // println!("Skip List:\n{}", skip_list);
 
-    skip_list.edit(1, |i| *i = i.to_uppercase()).unwrap();
-    println!("Skip List:\n{}", skip_list);
+    skip_list.edit(1, |i| *i = ()).unwrap();
+    // println!("Skip List:\n{}", skip_list);
 
     skip_list.remove(9).unwrap();
-    println!("Skip List:\n{}", skip_list);
+    // println!("Skip List:\n{}", skip_list);
 
     // std::fs::write("skip_list.txt", format!("{}", skip_list))
     //     .map_err(|e| format!("Failed to write to file: {}", e))?;
